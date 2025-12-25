892a9f9809c9f5e61c2cb879502b329fc0c74546
Phufa  Srinatnava <phufa.s@ku.th>
2025-12-25 15:34:42 +0700

    Update calculator.test.js

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -2,7 +2,7 @@ const { add, subtract, multiply, divide } = require('../src/calculator');
 
 describe('Calculator', () => {
 
-    // ADD
+    // ADD avv
     test('adds 2 + 3 to equal 5', () => {
         expect(add(2, 3)).toBe(5);
     });
